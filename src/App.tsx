8ad24b31bf772279a39ac73ede8fd78ccf69d6d3
goodGai <1398281274@qq.com>
2023-06-13 22:24:17 +0800

    src目录的提交

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+function App () {
+  const [num, setNum] = useState(0)
+  const addNum = () => {
+      let tempNum = num
+      setNum(tempNum + 1)
+  }
+  return (
+    <div>
+      <div>{num}</div>
+      <button onClick={addNum}>点击+1</button>
+    </div>
+  )
+}
+
+
+export default App;
